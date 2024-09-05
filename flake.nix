d02127739adbb392d02799854d34ee954075bed2
haruki7049 <tontonkirikiri@gmail.com>
2024-09-05 18:25:36 +0900

    chore(repo): Init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, treefmt-nix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            # SBCL
+            pkgs.sbcl
+
+            # LSP
+            pkgs.nil
+          ];
+
+          shellHook = ''
+            export PS1="\n[nix-shell:\w]$ "
+          '';
+        };
+      });
+}
