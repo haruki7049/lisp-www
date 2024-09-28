bd61f408dff060162d7c2b6dada2abed8582c36b
haruki7049 <tontonkirikiri@gmail.com>
2024-09-28 14:57:58 +0900

    hoge

--- flake.nix
+++ flake.nix
@@ -1,91 +1,43 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      treefmt-nix,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        lib = pkgs.lib;
-        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+      ];
 
-        lisp = pkgs.sbcl;
-        lispPackages = pkgs.sbclPackages;
-        pname = "lisp-www";
-        version = "0.1.0";
-        src = lib.cleanSource ./.;
-        lispLibs = with lispPackages; [
-          woo
-        ];
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
 
-        lisp-www = lisp.buildASDFSystem {
-          inherit
-            pname
-            version
-            src
-            lispLibs
-            ;
-        };
+      perSystem =
+        { pkgs, lib, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.actionlint.enable = true;
+          };
 
-        runtime = pkgs.sbcl.withPackages (ps: [
-          ps.woo
-          lisp-www
-        ]);
+          devShells.default = pkgs.mkShell {
+            packages = [
+              # SBCL
+              pkgs.sbcl
 
-        runner =
-          let
-            dependencies = [
-              "woo"
-              "lisp-www"
+              # LSP
+              pkgs.nil
             ];
 
-            asdf-loader = lib.strings.concatLines (map (x: "(asdf:load-system '${x})") dependencies);
-          in
-          pkgs.writeScriptBin "runner" ''
-            #! ${runtime}/bin/sbcl --script
-            (load (sb-ext:posix-getenv "ASDF"))
-            ${asdf-loader}
-
-            (lisp-www:main)
-          '';
-      in
-      {
-        formatter = treefmtEval.config.build.wrapper;
-
-        packages = {
-          inherit lisp-www runner;
-          default = runner;
+            shellHook = ''
+              export PS1="\n[nix-shell:\w]$ "
+            '';
+          };
         };
-
-        checks = {
-          inherit lisp-www runtime runner;
-          formatting = treefmtEval.config.build.check self;
-        };
-
-        devShells.default = pkgs.mkShell {
-          packages = [
-            # SBCL
-            lisp
-
-            # LSP
-            pkgs.nil
-          ];
-
-          shellHook = ''
-            export PS1="\n[nix-shell:\w]$ "
-          '';
-        };
-      }
-    );
+    };
 }
--- treefmt.nix
+++
@@ -1,5 +0,0 @@
-{
-  projectRootFile = "flake.nix";
-  programs.nixfmt-rfc-style.enable = true;
-  programs.actionlint.enable = true;
-}
